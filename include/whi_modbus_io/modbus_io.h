0ea8f491551228f73679cd7671365922720bffca
Xinjue <hit_zou@126.com>
2023-09-06 17:36:24 +0800

    integrate serial

--- include/whi_modbus_io/modbus_io.h
+++ include/whi_modbus_io/modbus_io.h
@@ -5,6 +5,10 @@ Features:
 - modbus
 - xxx
 
+Dependencies:
+- sudo apt install ros-<ros distro>-serial
+- sudo usermod -a -G dialout <user name>, the reboot
+
 Written by Xinjue Zou, xinjue.zou@outlook.com
 
 Apache License Version 2.0, check LICENSE for more information.
@@ -16,6 +20,7 @@ Changelog:
 ******************************************************************/
 #pragma once
 #include <ros/ros.h>
+#include <serial/serial.h>
 
 #include <memory>
 
@@ -24,6 +29,7 @@ namespace whi_modbus_io
 	class ModbusIo
 	{
     public:
+        ModbusIo() = delete;
         ModbusIo(std::shared_ptr<ros::NodeHandle>& NodeHandle);
         ~ModbusIo();
 
@@ -32,5 +38,9 @@ namespace whi_modbus_io
 
     protected:
         std::shared_ptr<ros::NodeHandle> node_handle_{ nullptr };
+        std::string module_;
+	    std::string serial_port_;
+	    int baudrate_{ 9600 };
+        std::unique_ptr<serial::Serial> serial_inst_{ nullptr };
 	};
 } // namespace whi_modbus_io
--- src/sdk/modbus_io.cpp
+++ src/sdk/modbus_io.cpp
@@ -5,6 +5,10 @@ Features:
 - modbus
 - xxx
 
+Dependencies:
+- sudo apt install ros-<ros distro>-serial
+- sudo usermod -a -G dialout <user name>, the reboot
+
 Written by Xinjue Zou, xinjue.zou@outlook.com
 
 Apache License Version 2.0, check LICENSE for more information.
@@ -29,6 +33,18 @@ namespace whi_modbus_io
     void ModbusIo::init()
     {
         // params
+        node_handle_->param("hardware_interface/module", module_, std::string());
+        node_handle_->param("hardware_interface/port", serial_port_, std::string("/dev/ttyUSB0"));
+        node_handle_->param("hardware_interface/baudrate", baudrate_, 9600);
 
+        // serial
+	    try
+	    {
+		    serial_inst_ = std::make_unique<serial::Serial>(serial_port_, baudrate_, serial::Timeout::simpleTimeout(500));
+	    }
+	    catch (serial::IOException& e)
+	    {
+		    ROS_FATAL_STREAM_NAMED("failed to open serial %s", serial_port_.c_str());
+	    }
     }
 } // namespace whi_modbus_io
